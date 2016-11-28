8783027817ee7c17001bc42c8b2aed2332aa6af6
Mykhailo Iartysh <mykhailo_iartysh@epam.com>
2016-11-28 18:25:19 +0200

    created main structure

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var path = require('path')
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: "bundle.js"
+  },
+  module: {
+  loaders: [
+    { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+  ]
+}
+};
